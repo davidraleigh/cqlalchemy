35b8695169ce76d8a43311cd610e369737624b1f
David Raleigh <davidraleigh@gmail.com>
2024-11-10 18:29:35 -0800

    cleanup in Enum build

--- src/cqlalchemy/scaffold/build.py
+++ src/cqlalchemy/scaffold/build.py
@@ -2,6 +2,8 @@ import pkgutil
 from string import Template
 
 enum_template = Template(pkgutil.get_data(__name__, "templates/enum.template").decode('utf-8'))
+ENUM_KEY_VALUES = "    {x} = \"{x}\"\n"
+ENUM_QUERY_CLASS = "\n    def {x}(self) -> QueryBlock:\n        return self.equals({class_name}.{x})\n"
 
 
 def build_enum(enum_field_key: str, enum_object: dict, full_name=False, add_unique=False):
@@ -12,11 +14,12 @@ def build_enum(enum_field_key: str, enum_object: dict, full_name=False, add_uniq
         enum_field_key = enum_field_key.split(":")[1]
     class_name = prefix + "".join([x.capitalize() for x in enum_field_key.split("_")])
 
-    enum_key_values = "\n".join([f"    {x} = \"{x}\"" for x in enum_object["enum"]])
+    enum_key_values = ""
     custom_methods = ""
-    if add_unique:
-        custom_methods = "\n"
-        custom_methods += "\n".join([f"    def {x}(self) -> QueryBlock:\n        return self.equals({class_name}.{x})\n" for x in enum_object["enum"]])
+    for x in enum_object["enum"]:
+        enum_key_values += ENUM_KEY_VALUES.format(x=x)
+        if add_unique:
+            custom_methods += ENUM_QUERY_CLASS.format(x=x, class_name=class_name)
 
     return enum_template.substitute(class_name=class_name,
                                     enum_key_values=enum_key_values,
