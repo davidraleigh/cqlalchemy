4db5878e74010993f608fa6e6f81a931804fdf9e
David Raleigh <davidraleigh@gmail.com>
2024-11-13 23:03:34 -0800

    more hidden classes and methods

--- src/cqlalchemy/scripts/main.py
+++ src/cqlalchemy/scripts/main.py
@@ -4,7 +4,7 @@ from datetime import date, datetime, timedelta
 from shapely import Point
 
 from cqlalchemy.stac import query as q
-from cqlalchemy.stac.query import DateTimeEncoder, QueryBuilder, filter_grouping
+from cqlalchemy.stac.query import QueryBuilder, _DateTimeEncoder, filter_grouping
 
 if __name__ == '__main__':
     import uuid
@@ -22,7 +22,7 @@ if __name__ == '__main__':
     query.filter((query.sar.resolution_azimuth == 45.3) & (query.datetime == 55.3) & (query.id == "stuff.3") &
                  (query.created >= datetime.now()) & (query.created <= datetime.now()) &
                  filter_grouping((query.sar.resolution_azimuth >= 90.3) | (query.id == "pancakes.3")))
-    print(json.dumps(query.build_query(), indent=4, cls=DateTimeEncoder))
+    print(json.dumps(query.build_query(), indent=4, cls=_DateTimeEncoder))
 
     print(QueryBuilder().sar.resolution_azimuth.gt(99).sar.resolution_azimuth.lt(1).build_query())
     print(QueryBuilder().filter(query.sar.resolution_azimuth > 100))
